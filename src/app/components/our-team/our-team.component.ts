31bf6a6c1bb302efa780fb58dc80ae3ef59d2378
pavani <pavani@gmail.com>
2025-07-16 12:12:02 +0530

    fifteenth

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -12,28 +12,28 @@ export class OurTeamComponent {
       // designation: 'Chief Neuroscientist',
       description:
         'Rehabilitation involves a multidisciplinary team providing therapy to improve daily functioning. Cognitive Rehabilitation Therapy (CRT) offers individualized programs to restore brain function and manage deficits, requiring further development and evaluation for effectiveness.',
-      image: 'assets/intro-pic.png',
+      image: 'assets/neurologist-team.jpg',
     },
     {
       name: 'Radiologist ',
       // designation: 'Lead Neurologist',
       description:
         'Doctors use fMRI to understand brain function, assess surgical risks, and evaluate stroke, trauma, or degenerative disease effects. The FDA approves fMRI mainly for surgical planning, helping to map brain functions and monitor tumors.',
-      image: 'assets/fmri-pic.png',
+      image: 'assets/radiologist-team.jpeg',
     },
     {
       name: 'Physiatrist',
       // designation: 'Clinical Psychologist',
       description:
         'Active involvement in stroke rehabilitation is beneficial. Encourage participation in therapy sessions to aid recovery while promoting independence. Assess and meet the survivors evolving needs through medical team consultations and home adjustments.',
-      image: 'assets/stroke-rehabilitation.png',
+      image: 'assets/physiatrists-team.webp',
     },
     {
       name: 'Epidemiologist',
       // designation: 'Research Fellow',
       description:
         'Traumatic brain injury is a major global health issue, leading to death and disability. Mortality rates vary by severity, age, and geographical location. Effective prevention strategies and access to rehabilitation services are critical for improving outcomes worldwide.',
-      image: 'assets/epidemiology-pic.png',
+      image: 'assets/epidemiologist-team.jpg',
     },
     {
       name: 'Music Therapist',
